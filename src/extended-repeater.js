6d46acf9f1e264901de3063b680811ca8d449703
KateMikol <gornovskayakt@gmail.com>
2023-04-13 23:16:41 +0300

    start repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -9,16 +9,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-function repeater(/* str, options */) {
+ * */
+  repeater('STRING', options = { repeatTimes: 3, separator: '**', 
+  addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+// => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'*/
+ 
+ 
+function repeater(str, options) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let addSep = '|';
+  if (!!options.additionSeparator){
+     addSep = String(options.additionSeparator);
+  }else if(options.additionSeparator == false){
+     addSep = 'false';
+  }
+  let tailArr = new Array(!!options.additionRepeatTimes ? Number(options.additionRepeatTimes) : 1 ).fill(!!options.addition ? String(options.addition): '');
+  let tail = tailArr.join(`${addSep}`);
+  let newStr = new Array(!!options.repeatTimes ? Number(options.repeatTimes) : 1).fill(String(str) + tail) ;
+
+  // console.log(Number(options.additionRepeat));
+  // console.log(Number(options.repeatTimes));
+  
+
+  return newStr.join(`${!!options.separator ? String(options.separator) : '+'}`);
+ 
+
 }
+console.log(String(options.additionSeparator));
+// console.log(String(false).type());
 
 module.exports = {
   repeater
